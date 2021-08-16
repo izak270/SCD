f0833ed1104e5fed139af550231180a3fcc28730
izak270 <58972658+izak270@users.noreply.github.com>
2021-08-16 09:51:37 +0300

    all run good but return nan from olga and ron file

--- main/Main.py
+++ main/Main.py
@@ -1,19 +1,19 @@
 from pre_process_utils import convert_origin_2_raw_data, convert_raw_data_2_data_frame, convert_df_2_pkl, \
   bert_w2v_mapper, finally_concat_pickles
 import create_vectors_text_meta_voice_with_predictions, hparam
-import new_d_vectors2, hparam, kmeans_m
+import new_d_vectors2
 from hparam import hparam as hp
 
 
 def main():
-  # convert_origin_2_raw_data()
-  # convert_raw_data_2_data_frame()
-  # convert_df_2_pkl()
-  # bert_w2v_mapper()
-  # finally_concat_pickles()
-  # create_vectors_text_meta_voice_with_predictions.create_labels_df_from_vectors()
-  # new_d_vectors2.run_D_vectors()
-  kmeans_m
+  print('start12121212')
+  convert_origin_2_raw_data()
+  convert_raw_data_2_data_frame()
+  convert_df_2_pkl()
+  bert_w2v_mapper()
+  finally_concat_pickles()
+  create_vectors_text_meta_voice_with_predictions.create_labels_df_from_vectors()
+  new_d_vectors2.run_D_vectors()
   return
 
 if __name__ == "__main__":
