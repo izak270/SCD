4a02b76696949b9439514bccdf42bd3ae159a755
Imanuel Eyal <hodiya.eyal@gmail.com>
2021-08-21 20:19:58 +0300

    Removing function calls from pre process utils

--- main/pre_process_utils.py
+++ main/pre_process_utils.py
@@ -272,18 +272,7 @@ def finally_concat_pickles():
   print("Finish to concatenate Pickles")
   return
 
-  from pre_process_utils import convert_origin_2_raw_data
-
-def main():
-  convert_origin_2_raw_data()
-  convert_raw_data_2_data_frame()
-  convert_df_2_pkl()
-  bert_w2v_mapper()
-  finally_concat_pickles()
-  return
-
-if __name__ == "__main__":
-    main()
+ 
 
 
 
