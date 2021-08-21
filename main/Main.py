4a452c9db006ce86a15106218419b99a4edb4018
Imanuel Eyal <hodiya.eyal@gmail.com>
2021-08-21 19:49:18 +0300

    Adding the prediction file import the the matched function

--- main/Main.py
+++ main/Main.py
@@ -1,6 +1,7 @@
 from pre_process_utils import convert_origin_2_raw_data, convert_raw_data_2_data_frame, convert_df_2_pkl, \
   bert_w2v_mapper, finally_concat_pickles
-import create_vectors_text_meta_voice_with_predictions, hparam, new_d_vectors2, kmeans_m
+import create_vectors_text_meta_voice, hparam, new_d_vectors2, kmeans_m
+import get_predictions
 from hparam import hparam as hp
 
 
@@ -11,7 +12,8 @@ def main():
   convert_df_2_pkl()
   bert_w2v_mapper()
   finally_concat_pickles()
-  create_vectors_text_meta_voice_with_predictions.create_vectors_from_preprocessed_data()
+  create_vectors_text_meta_voice.create_vectors_from_preprocessed_data()
+  get_predictions.get_predictions()
   new_d_vectors2.run_D_vectors()
   kmeans_m.startk()
   return
